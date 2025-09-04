260595501911ae06ae013d3d327629022ec4c73d
Al amin <alaminmridha2004@gmail.com>
2025-09-04 06:14:46 +0600

    Sidebar add in Shops page

--- restaurant-app/src/app/(pages)/shops/page.tsx
+++ restaurant-app/src/app/(pages)/shops/page.tsx
@@ -5,15 +5,18 @@ import Image from "next/image";
 import CardBoxForStore from "./_components/CardBoxForStore";
 
 import { allStores } from "@/datas/stores"
+import Sidebar from "@/app/_component/shared/sidebar/Sidebar";
 
 export default function Shops() {
   return (
     <div className="container relative mx-auto xl:grid grid-cols-9 gap-5 2xl:gap-12">
-      <aside className="hidden xl:block col-span-2">
+      <div className="hidden xl:block col-span-2">
         <div className="sticky top-[122px] p-4 h-[calc(100vh_-_122px)]">
-          <div className="size-full border border-fp-gray rounded-2xl"></div>
+          <div className="size-full border border-fp-gray rounded-2xl overflow-y-scroll">
+            <Sidebar/>
+          </div>
         </div>
-      </aside>
+      </div>
 
       <div className="px-4 col-span-7 mt-5 md:pt-8">
         {/* <SearchBox /> */}
