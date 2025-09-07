b33347af9a1f1df9332cbd2514818e06b9f64719
Al amin <alaminmridha2004@gmail.com>
2025-09-07 19:39:15 +0600

    UI update user of Content dropdown

--- restaurant-app/src/app/_component/shared/navbar/_components/UserContent.tsx
+++ restaurant-app/src/app/_component/shared/navbar/_components/UserContent.tsx
@@ -12,12 +12,7 @@ import {
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuPortal,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import {
@@ -172,56 +167,25 @@ const UserContent = () => {
                 <ChevronDown color="var(--primary)" />
               </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-64" align="start">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuGroup>
-                <DropdownMenuItem>
-                  Profile
-                  <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  Billing
-                  <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  Settings
-                  <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  Keyboard shortcuts
-                  <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
+            <DropdownMenuContent className="w-75 p-3" align="start">
+              <DropdownMenuLabel className="text-lg">My Account</DropdownMenuLabel>
+              <DropdownMenuItem className="text-base hover:bg-secondary/15 p-3 px-4 cursor-pointer">Profile</DropdownMenuItem>
               <DropdownMenuSeparator />
               <DropdownMenuGroup>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuSub>
-                  <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
-                  <DropdownMenuPortal>
-                    <DropdownMenuSubContent>
-                      <DropdownMenuItem>Email</DropdownMenuItem>
-                      <DropdownMenuItem>Message</DropdownMenuItem>
-                      <DropdownMenuSeparator />
-                      <DropdownMenuItem>More...</DropdownMenuItem>
-                    </DropdownMenuSubContent>
-                  </DropdownMenuPortal>
-                </DropdownMenuSub>
-                <DropdownMenuItem>
-                  New Team
-                  <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
-                </DropdownMenuItem>
+                <DropdownMenuItem className="text-base hover:bg-secondary/15 p-3 px-4 cursor-pointer">Pandapay</DropdownMenuItem>
+                <DropdownMenuItem className="text-base hover:bg-secondary/15 p-3 px-4 cursor-pointer">Subscribe to free Delivery</DropdownMenuItem>
+                <DropdownMenuItem className="text-base hover:bg-secondary/15 p-3 px-4 cursor-pointer">Orders and Recording</DropdownMenuItem>
+                <DropdownMenuItem className="text-base hover:bg-secondary/15 p-3 px-4 cursor-pointer">Vouchers</DropdownMenuItem>
+                <DropdownMenuItem className="text-base hover:bg-secondary/15 p-3 px-4 cursor-pointer">Panda rewards</DropdownMenuItem>
               </DropdownMenuGroup>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>GitHub</DropdownMenuItem>
-              <DropdownMenuItem>Support</DropdownMenuItem>
-              <DropdownMenuItem disabled>API</DropdownMenuItem>
+              <DropdownMenuItem className="text-base hover:bg-secondary/15 p-3 px-4 cursor-pointer">Help Center </DropdownMenuItem>
               <DropdownMenuSeparator />
               <DropdownMenuItem
                 onClick={() => signOut()}
-                className="cursor-pointer"
+                className="text-base hover:bg-secondary/15 p-3 px-4 cursor-pointer"
               >
                 Log out
-                <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
